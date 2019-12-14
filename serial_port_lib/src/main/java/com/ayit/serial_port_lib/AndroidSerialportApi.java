bbf35e9783795597c76d72d24001306f2a48fdd2
lny <1053452188@qq.com>
2019-12-14 14:23:45 +0800

    串口通信处理负数问题

--- serial_port_lib/src/main/java/com/ayit/serial_port_lib/AndroidSerialportApi.java
+++ serial_port_lib/src/main/java/com/ayit/serial_port_lib/AndroidSerialportApi.java
@@ -519,4 +519,75 @@ public class AndroidSerialportApi {
         }
     }
 
+    public static short hex2short(String hexH,String hexL) throws Exception {
+        if (hexH.length()!=2 || hexL.length()!=2){
+            throw new Exception("hexString lenght must be 2");
+        }
+        short i =binaryToShort(byte2BinaryString(HexToByte(hexH))+byte2BinaryString(HexToByte(hexL)));
+        short shortI = (short)i;
+        return shortI;
+    }
+
+    public static String[] short2hex(short number){
+        String hexH = Byte2Hex((byte)(number>>8));
+        String hexL = Byte2Hex((byte)((number<<8)>>8));
+        String[] ss = new String[2];
+        ss[0] = hexH;
+        ss[1] = hexL;
+        return ss;
+    }
+
+    public static String byte2BinaryString(byte b){
+        return Integer.toBinaryString((b & 0xFF) + 0x100).substring(1);
+    }
+    public static short binaryToShort(String binary){
+        return (short)binaryToInt(binary);
+    }
+    public static int binaryToInt(String binary) {
+        if (binary == null) {
+            System. out.println("can't input null ！" );
+        }
+        if (binary.isEmpty()) {
+            System. out.println("you input is Empty !" );
+        }
+        int max = binary.length();
+        String new_binary = "";
+        if (max >= 2 && binary.startsWith("0")) {
+            int position = 0;
+            for (int i = 0; i < binary.length(); i++) {
+                char a = binary.charAt(i);
+                if (a != '0' ) {
+                    position = i;
+                    break;
+                }
+            }
+            if (position == 0) {
+                new_binary = binary.substring(max - 1, max);
+            } else {
+                new_binary = binary.substring(position, max);
+            }
+        } else {
+            new_binary = binary;
+        }
+        int new_width = new_binary.length();
+
+        long result = 0;
+        if (new_width < 32) {
+            for (int i = new_width; i > 0; i--) {
+                char c = new_binary.charAt(i - 1);
+                int algorism = c - '0' ;
+                result += Math. pow(2, new_width - i) * algorism;
+            }
+        } else if (new_width == 32) {
+            for (int i = new_width; i > 1; i--) {
+                char c = new_binary.charAt(i - 1);
+                int algorism = c - '0' ;
+                result += Math. pow(2, new_width - i) * algorism;
+            }
+            result += -2147483648;
+        }
+        int a = new Long(result).intValue();
+        return a;
+    }
+
 }
